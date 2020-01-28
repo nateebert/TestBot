e56d1d9607374d86e57e7e768bc8f3fe5f141b9e
nateebert <60366036+nateebert@users.noreply.github.com>
2020-01-27 20:56:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,18 @@ from flask import Flask, request
 #define our flask app
 app = Flask(__name__)
 
+#Sends a message to the chat that the bot originates from
+def send_msg(msg):
+
+  url  = 'https://api.groupme.com/v3/bots/post'
+  
+  data ={
+  'bot_id' : os.getenv('GROUPME_BOT_ID'),
+  'text'   : msg
+  }
+        
+  request = requests.post(url=url, data=data)
+
 #Method will automatically execute when our endpoint receives a POST call
 @app.route('/', methods=['POST'])
 def msg_received_from_group():
@@ -30,18 +42,6 @@ def msg_received_from_group():
 
   return "ok", 200
 
- 
-#Sends a message to the chat that the bot originates from
-def send_msg(msg):
-
-  url  = 'https://api.groupme.com/v3/bots/post'
-  
-  data ={
-  'bot_id' : os.getenv('GROUPME_BOT_ID'),
-  'text'   : msg
-  }
-        
-  request = requests.post(url=url, data=data)
 
 #sends a picture and a message to the chat
 #Picture URL must be registered with GroupMe first

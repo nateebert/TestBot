75e979948a68c12d537be3363e0e9e9b5757fb1c
nateebert <60366036+nateebert@users.noreply.github.com>
2020-01-30 16:47:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def msg_received_from_group():
   log('{}'.format(data))
   
   #Check the text of the message sent to the chat to see if it matches our command word
-  if data['name'].lower() == "blorp":
+  if data['name'].lower() == 'blorp':
     n = random()
     if n<=0.25:
       send_msg("cork it, space trash")
@@ -44,7 +44,7 @@ def msg_received_from_group():
       send_msg("hey. hey jason. quiet.")
     else:
       send_msg("jason your face is the visual equivalent of the f-word")
-  elif data['text'].lower() == "!test":
+  elif data['text'].lower() == '!test':
     send_msg("all set boss")
 
 
